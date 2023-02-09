dc7c0936cc49d4fc0d5ef00989ec080e7968c399
Alexey Besedin <cdr800mb@mail.ru>
2023-02-09 13:12:44 +0400

    Fix style issues

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,5 +3,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
         Cli.greeting();
-    }    
+    }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public static void greeting(){
+    public static void greeting() {
         Scanner console  = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String name = console.nextLine();
-        System.out.println("Hello, "+ name + "!");
+        System.out.println("Hello, " + name + "!");
         console.close();
     }
 }
