8e10c619c88e286d20dd12ba34c296e847f9ab74
Alexey Besedin <cdr800mb@mail.ru>
2023-02-11 19:45:44 +0400

    Change name logic, remove close for scanner

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class Cli {
 
     public static void greeting() {
-        Scanner console  = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
         System.out.print("May I have your name? ");
-        String name = console.nextLine();
-        System.out.println("Hello, " + name + "!");
-        console.close();
+        App.playerName = console.nextLine();
+        System.out.println("Hello, " + App.playerName + "!");
     }
 }
