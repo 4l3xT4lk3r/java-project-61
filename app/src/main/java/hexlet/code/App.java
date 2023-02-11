4cba0e33fff6e3c8805e5cfe34da1aac0525c8d9
Alexey Besedin <cdr800mb@mail.ru>
2023-02-11 19:44:34 +0400

    Modify menu, add Even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
+
+import java.util.Scanner;
+
 public class App {
+    public static String playerName;
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        switch (console.nextInt()) {
+            case (1):
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greeting();
+                break;
+            case (2):
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greeting();
+                Even.game();
+                break;
+            default:
+                break;
+        }
+        console.close();
     }
 }
