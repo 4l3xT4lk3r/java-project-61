960cc3d234fdb2e84c7fb6477b4b28b62718a2de
Alexey Besedin <cdr800mb@mail.ru>
2023-02-13 01:02:46 +0400

    Refactor Even class

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -3,28 +3,24 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Even {
-    public static void game() {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        for (int i = 0; i < 3; i++) {
-            int number = (int) (Math.random() * 100);
-            String correctAnswer;
-            if (number % 2 == 0) {
-                correctAnswer = "yes";
-            } else {
-                correctAnswer = "no";
-            }
-            System.out.println("Question: " + number);
-            System.out.print("Your answer: ");
-            String answer = console.next();
-            if (!answer.equals(correctAnswer)) {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
-                System.out.println("Let's try again, " + App.playerName);
-                return;
-            } else {
-                System.out.println("Correct!");
-            }
+    public static final String gameDescription = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    public static boolean game(Scanner console) {
+        int number = (int) (Math.random() * 100);
+        String correctAnswer;
+        if (number % 2 == 0) {
+            correctAnswer = "yes";
+        } else {
+            correctAnswer = "no";
         }
-        System.out.println("Congratulations, " + App.playerName + "!");
+        System.out.println("Question: " + number);
+        System.out.print("Your answer: ");
+        String answer = console.next();
+        if (!answer.equals(correctAnswer)) {
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+            return false;
+        } else {
+            System.out.println("Correct!");
+        }
+        return true;
     }
 }
