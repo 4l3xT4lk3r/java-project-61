cf5f9cc8c07e7d41ef5dfcb01222b2e5153033b2
Alexey Besedin <cdr800mb@mail.ru>
2023-02-12 13:24:50 +0400

    Improving App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,27 +1,37 @@
 package hexlet.code;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
     public static String playerName;
+
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("0 - Exit");
-        switch (console.nextInt()) {
-            case (1):
-                System.out.println("Welcome to the Brain Games!");
-                Cli.greeting();
-                break;
-            case (2):
-                System.out.println("Welcome to the Brain Games!");
-                Cli.greeting();
+        System.out.print("Your choice: ");
+        int pick;
+        try {
+            pick = console.nextInt();
+        } catch (InputMismatchException exception) {
+            System.out.println("Wrong pick!");
+            return;
+        }
+        if (pick >= 1 && pick <= 6) {
+            System.out.println("\nWelcome to the Brain Games!");
+            Cli.greeting();
+        }
+        switch (pick) {
+            case (0), (1) -> {
+                return;
+            }
+            case (2) -> {
                 Even.game();
-                break;
-            default:
-                break;
+            }
+            default -> System.out.println("Wrong pick!");
         }
         console.close();
     }
