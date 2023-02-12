2968dacc150f4358246cf0d34f861374c04ac072
Alexey Besedin <cdr800mb@mail.ru>
2023-02-13 01:04:12 +0400

    Refactor Cli class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-
-    public static void greeting() {
-        Scanner console = new Scanner(System.in);
+    public static String playerName;
+    public static void greeting(Scanner console) {
         System.out.print("May I have your name? ");
-        App.playerName = console.nextLine();
-        System.out.println("Hello, " + App.playerName + "!");
+        playerName = console.nextLine();
+        System.out.println("Hello, " + playerName + "!");
     }
 }
