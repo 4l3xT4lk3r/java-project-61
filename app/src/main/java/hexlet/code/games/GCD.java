f1d3f229846e89e1d32e379b15d6ff21cebfccc3
Alexey Besedin <cdr800mb@mail.ru>
2023-02-18 01:56:55 +0400

    Refactor GCD game

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,33 +1,38 @@
 package hexlet.code.games;
 
-import java.util.Scanner;
+import java.util.Random;
 
-public class GCD {
-    public static final String GAME_DESCRIPTION = "Find the greatest common divisor of given numbers.";
+public class GCD implements Playable {
+    public final String gameDescription = "Find the greatest common divisor of given numbers.";
+    private String question;
+    private String answer;
 
-    public static boolean game(Scanner console) {
-        int a = (int) (Math.random() * 100);
-        int b = (int) (Math.random() * 100);
+    @Override
+    public String getGameDescription() {
+        return gameDescription;
+    }
+
+    @Override
+    public void playGame() {
+        int a = new Random().nextInt(1, 100);
+        int b = new Random().nextInt(1, 100);
+        question = a + " " + b;
         int gdc = 1;
         for (int i = 1; i <= Math.min(a, b); i++) {
             if (a % i == 0 && b % i == 0) {
                 gdc = i;
             }
         }
-        System.out.println("Question: " + a + " " + b);
-        System.out.print("Your answer: ");
-        String answer = console.next();
-        try {
-            if (Integer.parseInt(answer) != gdc) {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + gdc + "'.");
-                return false;
-            } else {
-                System.out.println("Correct!");
-            }
-        } catch (NumberFormatException exception) {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + gdc + "'.");
-            return false;
-        }
-        return true;
+        answer = "" + gdc;
+    }
+
+    @Override
+    public String getQuestion() {
+        return question;
+    }
+
+    @Override
+    public String getAnswer() {
+        return answer;
     }
 }
